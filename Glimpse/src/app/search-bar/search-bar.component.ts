5e13a77a165d744d406379a8f073a01ffe0fd025
Kevin Capener <kevincapener@gmail.com>
2024-08-14 13:25:39 -0700

    basic reactive form set up

--- Glimpse/src/app/search-bar/search-bar.component.ts
+++ Glimpse/src/app/search-bar/search-bar.component.ts
@@ -1,18 +1,30 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormGroup, FormControl } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-search-bar',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './search-bar.component.html',
   styleUrl: './search-bar.component.css'
 })
 export class SearchBarComponent {
-  
+
   constructor(private router: Router) { }
+
+  searchForm = new FormGroup(
+    {
+      search: new FormControl(''),
+    }
+  );
 
   navigateToList() {
     this.router.navigate(['/list']);
+  }
+
+  handleSubmit() {
+    alert(this.searchForm.value.search);
   }
 }
