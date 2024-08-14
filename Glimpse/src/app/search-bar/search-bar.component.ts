d81e2b4c1560f89d02953f8e57cea03ddc20c4e1
Kevin Capener <kevincapener@gmail.com>
2024-08-14 13:41:44 -0700

    added validation to search form

--- Glimpse/src/app/search-bar/search-bar.component.ts
+++ Glimpse/src/app/search-bar/search-bar.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormGroup, FormControl } from '@angular/forms';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-search-bar',
@@ -14,17 +14,21 @@ export class SearchBarComponent {
 
   constructor(private router: Router) { }
 
-  searchForm = new FormGroup(
-    {
-      search: new FormControl(''),
-    }
-  );
+  searchForm = new FormGroup({
+    search: new FormControl('', [Validators.required, Validators.minLength(3)]),
+  });
 
   navigateToList() {
     this.router.navigate(['/list']);
   }
 
   handleSubmit() {
+    if (!this.searchForm.valid) {
+      alert("Form is invalid on submit");
+      return;
+    }
+    // handle searching
+    // use EventEmitter with form value?
     alert(this.searchForm.value.search);
   }
 }
