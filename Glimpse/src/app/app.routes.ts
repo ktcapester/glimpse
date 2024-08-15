ff9633c8f7a8d01453ce81b2aa730e3679dc4ede
Kevin Capener <kevincapener@gmail.com>
2024-08-15 01:43:49 -0700

    made a temp component to lay out the search result screen

--- Glimpse/src/app/app.routes.ts
+++ Glimpse/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from "@angular/router";
 import { SearchStartComponent } from "./search-start/search-start.component";
 import { CardListComponent } from "./card-list/card-list.component";
 import { SearchResultComponent } from "./search-result/search-result.component";
+import { TempresultsComponent } from "./tempresults/tempresults.component";
 
 export const routes: Routes = [
     {
@@ -18,6 +19,11 @@ export const routes: Routes = [
         path: 'result/:cardName',
         title: 'Glimpse',
         component: SearchResultComponent,
+    },
+    {
+        path: 'temp',
+        title: 'Glimpse',
+        component: TempresultsComponent,
     },
     {
         path: '**',
---
+++ Glimpse/src/app/tempresults/tempresults.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from "../header/header.component";
+
+@Component({
+  selector: 'app-tempresults',
+  standalone: true,
+  imports: [HeaderComponent],
+  templateUrl: './tempresults.component.html',
+  styleUrl: './tempresults.component.css'
+})
+export class TempresultsComponent {
+
+}
