9aa7ad3372a41e50954605e29cfe30f55d61106f
Kevin Capener <kevincapener@gmail.com>
2024-08-20 00:32:51 -0700

    added routing from search bar to result.

--- Glimpse/src/app/search-bar/search-bar.component.ts
+++ Glimpse/src/app/search-bar/search-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormGroup, FormControl } from '@angular/forms';
 import { ReactiveFormsModule, Validators } from '@angular/forms';
@@ -11,9 +11,19 @@ import { ScryfallSearchService } from '../services/scryfall-search.service';
   templateUrl: './search-bar.component.html',
   styleUrl: './search-bar.component.css'
 })
-export class SearchBarComponent {
+export class SearchBarComponent implements OnInit {
+
+  private card$ = this.searchService.getResultCardUpdateListener();
 
   constructor(private router: Router, private searchService: ScryfallSearchService) { }
+
+  ngOnInit(): void {
+    this.card$.subscribe(
+      (nextData) => {
+        this.router.navigate(['/result', nextData.name]);
+      });
+  }
+
 
   searchForm = new FormGroup({
     search: new FormControl('', [Validators.required, Validators.minLength(3)]),
@@ -25,24 +35,13 @@ export class SearchBarComponent {
 
   handleSubmit() {
     if (!this.searchForm.valid) {
-      alert("Form is invalid on submit");
+      // do nothing so they stay on the page
       return;
     }
-    // handle searching
-    alert(this.searchForm.value.search);
 
-    // start spinner
+    // start a spinner?
     // search for the card
-    this.searchService.findCard(this.searchForm.value.search)
-      .subscribe(
-        (responseData) => {
-          // if good result, route to /result with the result as input
-          this.router.navigate(['/result', responseData.name]);
-        },
-        (errorData) => {
-          // if bad result, stop spinner & notify
-          // 404 with either zero cards matched or more than 1 matched
-          console.log(errorData);
-        });
+    this.searchService.findCard(this.searchForm.value.search);
+
   }
 }
