38335e61d07c3e9cbb5ef223daaff918a8825896
Kevin Capener <kevincapener@gmail.com>
2024-08-21 02:03:42 -0700

    added Subjects to correctly update data stored in the state service

--- Glimpse/src/app/services/glimpse-state.service.ts
+++ Glimpse/src/app/services/glimpse-state.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { User } from '../models/user.model';
 import { ScryfallCard } from '../interfaces/scryfall-card.interface';
 import { CardList } from '../models/card-list.model';
+import { ScryfallList } from '../interfaces/scryfall-list.interface';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +12,12 @@ export class GlimpseStateService {
 
   user: User | null = null;
   searchedCard: ScryfallCard | null = null;
+  searchedPrints: ScryfallList | null = null;
   currentList: CardList | null = null;
+  private cardSubject = new BehaviorSubject<ScryfallCard|null>(null);
+  card$ = this.cardSubject.asObservable();
+  private printsSubject = new BehaviorSubject<ScryfallList|null>(null);
+  prints$ = this.printsSubject.asObservable();
 
   constructor() { }
 
@@ -18,11 +25,23 @@ export class GlimpseStateService {
     return this.user;
   }
 
+  setSearchedCard(card: ScryfallCard) {
+    this.cardSubject.next(card);
+  }
+
   getSearchedCard() {
-    return this.searchedCard;
+    return this.cardSubject.value;
   }
 
   getCurrentList() {
     return this.user?.getActiveList();
+  }
+
+  setSearchedPrints(prints: ScryfallList) {
+    this.printsSubject.next(prints);
+  }
+
+  getPrints() {
+    return this.printsSubject.value;
   }
 }
