eb8a4ae88d44e9cbd5ec082ace404b9e019ac2f4
Kevin Capener <kevincapener@gmail.com>
2024-08-22 00:37:15 -0700

    Now calculating the price and displaying it!

--- Glimpse/src/app/search-result/search-result.component.ts
+++ Glimpse/src/app/search-result/search-result.component.ts
@@ -5,11 +5,14 @@ import { ActivatedRoute } from '@angular/router';
 import { ScryfallCard } from '../interfaces/scryfall-card.interface';
 import { GlimpseStateService } from '../services/glimpse-state.service';
 import { ScryfallList } from '../interfaces/scryfall-list.interface';
+import { PriceCalculatorService } from '../services/price-calculator.service';
+import { Prices } from '../interfaces/prices.interface';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-search-result',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, CurrencyPipe],
   templateUrl: './search-result.component.html',
   styleUrl: './search-result.component.css'
 })
@@ -18,13 +21,13 @@ export class SearchResultComponent implements OnInit {
   displayCard: DisplayCard = {
     name: 'Bellowing Crier',
     imgsrc: '../../assets/blb-42-bellowing-crier.jpg',
-    normalprice: '0.02',
-    fancyprice: '0.06'
+    normalprice: 0.02,
+    fancyprice: 0.06
   };
   resultCard!: ScryfallCard | null;
   printsList!: ScryfallList | null;
 
-  constructor(private route: ActivatedRoute, private state: GlimpseStateService) {
+  constructor(private route: ActivatedRoute, private state: GlimpseStateService, private pricer: PriceCalculatorService) {
     const cardNameInput = this.route.snapshot.params['cardName'];
     console.log(cardNameInput);
   }
@@ -41,16 +44,27 @@ export class SearchResultComponent implements OnInit {
       prints => {
         this.printsList = prints;
         console.log("New prints list:", prints);
+        let data = prints?.data ?? [];
+        let prices = this.pricer.bigoldfunc(data);
+        console.log("Calculated Prices:", prices);
+        this.displayCard = this.updatePrices(this.displayCard, prices)
       }
     );
+  }
+
+  updatePrices(card: DisplayCard, prices: Prices) {
+    let result =  { ...card };
+    result.normalprice = prices.usd;
+    result.fancyprice = prices.usd_foil;
+    return result;
   }
 
   convertCard(scard: ScryfallCard | null) {
     let result: DisplayCard = {
       name: 'Bellowing Crier',
       imgsrc: '../../assets/blb-42-bellowing-crier.jpg',
-      normalprice: '0.02',
-      fancyprice: '0.06',
+      normalprice: 0.02,
+      fancyprice: 0.06,
     }
     if (scard) {
       let imgsrc = ''
@@ -63,8 +77,8 @@ export class SearchResultComponent implements OnInit {
       result = {
         name: scard.name,
         imgsrc: imgsrc,
-        normalprice: '0.00',
-        fancyprice: '0.00',
+        normalprice: 0.00,
+        fancyprice: 0.00,
       }
     }
     return result;
