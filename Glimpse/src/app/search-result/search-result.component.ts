3513705fcb1863c5c026f0a9e952d5e344491dab
Kevin Capener <kevincapener@gmail.com>
2024-10-31 13:28:14 -0700

    add support for directly going to a result page from a URL

--- Glimpse/src/app/search-result/search-result.component.ts
+++ Glimpse/src/app/search-result/search-result.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { DisplayCard } from '../interfaces/display-card.interface';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { GlimpseStateService } from '../services/glimpse-state.service';
 import { CommonModule, CurrencyPipe } from '@angular/common';
 import { Subscription } from 'rxjs';
+import { BackendGlueService } from '../services/backend-glue.service';
 
 @Component({
   selector: 'app-search-result',
@@ -17,16 +18,20 @@ export class SearchResultComponent implements OnInit, OnDestroy {
   displayCard!: DisplayCard;
 
   private backend$!: Subscription;
+  private cardNameFromRoute = '';
 
   constructor(
     private route: ActivatedRoute,
-    private state: GlimpseStateService
+    private router: Router,
+    private state: GlimpseStateService,
+    private glue: BackendGlueService
   ) {
-    const cardNameInput = this.route.snapshot.params['cardName'];
-    console.log(cardNameInput);
+    this.cardNameFromRoute = this.route.snapshot.params['cardName'];
+    console.log(this.cardNameFromRoute);
   }
 
   ngOnDestroy(): void {
+    sessionStorage.removeItem('lastSearchedCard');
     this.backend$.unsubscribe();
   }
 
@@ -34,8 +39,56 @@ export class SearchResultComponent implements OnInit, OnDestroy {
     this.backend$ = this.state.getBackendCardListener().subscribe((card) => {
       console.log(card);
       if (!card) {
-        console.log('Card from state.backendSubject was null');
+        console.log('Card from state.backendSubject was null, checking url');
+        if (this.cardNameFromRoute) {
+          this.glue
+            .getCardSearch(this.cardNameFromRoute)
+            .subscribe((response) => {
+              if (typeof response === 'string') {
+                // error response
+                this.state.setErrorMessage(response);
+                this.router.navigate(['/404']);
+              } else {
+                // successful response
+                // aka response is CardSearch obj
+                this.state.setBackendCard(response);
+                this.displayCard = {
+                  name: response.name,
+                  imgsrc: response.imgsrc,
+                  foilprice: response.usd_foil,
+                  normalprice: response.usd,
+                  etchedprice: response.usd_etched,
+                };
+                return;
+              }
+            });
+        }
+        console.log('no backend card, no URL card, probably an error');
         return;
+      }
+
+      if (this.cardNameFromRoute != card.name) {
+        this.glue
+          .getCardSearch(this.cardNameFromRoute)
+          .subscribe((response) => {
+            if (typeof response === 'string') {
+              // error response
+              this.state.setErrorMessage(response);
+              this.router.navigate(['/404']);
+            } else {
+              // successful response
+              // aka response is CardSearch obj
+              this.state.setBackendCard(response);
+              this.displayCard = {
+                name: response.name,
+                imgsrc: response.imgsrc,
+                foilprice: response.usd_foil,
+                normalprice: response.usd,
+                etchedprice: response.usd_etched,
+              };
+              return;
+            }
+          });
       }
       this.displayCard = {
         name: card.name,
