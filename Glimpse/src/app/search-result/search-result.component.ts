84b298a87bdf44c78aad61363737fd4f6972e22a
Kevin Capener <kevincapener@gmail.com>
2024-10-31 18:02:43 -0700

    refactored to use the new search service

--- Glimpse/src/app/search-result/search-result.component.ts
+++ Glimpse/src/app/search-result/search-result.component.ts
@@ -5,8 +5,8 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { GlimpseStateService } from '../services/glimpse-state.service';
 import { CommonModule, CurrencyPipe } from '@angular/common';
 import { Subject } from 'rxjs';
-import { BackendGlueService } from '../services/backend-glue.service';
 import { takeUntil } from 'rxjs/operators';
+import { SearchDataService } from '../services/search-data.service';
 
 @Component({
   selector: 'app-search-result',
@@ -17,20 +17,18 @@ import { takeUntil } from 'rxjs/operators';
 })
 export class SearchResultComponent implements OnInit, OnDestroy {
   displayCard!: DisplayCard;
-
-  private cardNameFromRoute = '';
   private destroy$ = new Subject<void>();
 
   constructor(
     private route: ActivatedRoute,
     private router: Router,
     private state: GlimpseStateService,
-    private glue: BackendGlueService
+    private searchdata: SearchDataService
   ) {}
 
   ngOnDestroy(): void {
     console.log('SearchResult ngOnDestroy called!', Date.now());
-    sessionStorage.removeItem('lastSearchedCard');
+    // this.searchdata.clearSearchResults(); // maybe not needed here?
     this.destroy$.next();
     this.destroy$.complete();
   }
@@ -39,49 +37,20 @@ export class SearchResultComponent implements OnInit, OnDestroy {
     console.log('SearchResult ngOnInit called!', Date.now());
     // Set up getting names out of the URL
     this.route.paramMap.pipe(takeUntil(this.destroy$)).subscribe((params) => {
-      const name = params.get('cardName');
-      this.cardNameFromRoute = name ? name : '';
+      const name = params.get('cardName') || '';
+      this.searchdata.updateSearchTerm(name);
     });
-    console.log(this.cardNameFromRoute);
 
-    this.state
-      .getBackendCardListener()
+    this.searchdata.searchResults$
       .pipe(takeUntil(this.destroy$))
       .subscribe((card) => {
-        console.log(card);
-        // 4 cases to check due to existence or lack thereof for card & URL name
-        if (
-          this.cardNameFromRoute &&
-          (!card || this.cardNameFromRoute !== card.name)
-        ) {
-          // Case 1 and Case 3 combined: Either no backend card but URL card exists,
-          // or backend card exists and doesn’t match the URL card
-          this.getCardFromBack(this.cardNameFromRoute);
-        } else if (!card && !this.cardNameFromRoute) {
-          // Case 2: No backend card, no URL card name
-          console.log('No backend card, no URL card, likely an error');
-          this.state.setErrorMessage('Card not found');
-          this.router.navigate(['/404']);
-        } else if (card) {
-          // Case 4: Backend card exists and matches (or no URL card name)
+        if (card) {
           this.setDisplayCard(card);
-        }
-      });
-  }
-
-  private getCardFromBack(cardName: string): void {
-    this.glue
-      .getCardSearch(cardName)
-      .pipe(takeUntil(this.destroy$))
-      .subscribe((response) => {
-        if (typeof response === 'string') {
-          // error response
-          this.state.setErrorMessage(response);
-          this.router.navigate(['/404']);
+          this.router.navigate(['/result', card.name]);
         } else {
-          // successful response, response is CardSearch object
-          this.state.setBackendCard(response);
-          this.setDisplayCard(response);
+          // card was null
+          this.state.setErrorMessage('Trouble with searchResults$');
+          this.router.navigate(['/404']);
         }
       });
   }
