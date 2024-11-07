f9e64303a2eb286294abce4d682e9a7bb754301d
Kevin Capener <kevincapener@gmail.com>
2024-11-07 13:44:21 -0800

    corrected names and added currentTotal to backend model

--- Glimpse/backend/models/cardlist.js
+++ Glimpse/backend/models/cardlist.js
@@ -1,7 +1,8 @@
-let tempStore = [{ id: 1, name: "Sample Data", price: 8.44 }];
+let tempStore = [];
+let currentTotal = 0.0;
 
 const getAllCards = () => {
-  return tempStore;
+  return { list: tempStore, currentTotal: currentTotal };
 };
 
 const addCard = (newData) => {
@@ -10,9 +11,10 @@ const addCard = (newData) => {
   const newId = tempStore.length + 1;
   const data = { id: newId, ...newData };
   tempStore.push(data);
+  currentTotal += data.price;
   console.log("stored as:", data);
 
-  return data;
+  return { data, currentTotal };
 };
 
 module.exports = { getAllCards, addCard };
