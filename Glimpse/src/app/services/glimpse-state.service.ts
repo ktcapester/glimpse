1f5428a272c21c6b61918f7f928660be1f3edfba
Kevin Capener <kevincapener@gmail.com>
2024-11-08 15:10:38 -0800

    Cleaned up unused code from GlimpseState

--- Glimpse/src/app/services/glimpse-state.service.ts
+++ Glimpse/src/app/services/glimpse-state.service.ts
@@ -1,34 +1,19 @@
 import { Injectable } from '@angular/core';
-import { User } from '../models/user.model';
-import { ScryfallCard } from '../interfaces/scryfall-card.interface';
-import { CardList } from '../models/card-list.model';
-import { ScryfallList } from '../interfaces/scryfall-list.interface';
 import { BehaviorSubject } from 'rxjs';
-import { CardSearch } from '../interfaces/backend.interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class GlimpseStateService {
   // internals
-  private userSubject = new BehaviorSubject<User | null>(null);
-  private userListSubject = new BehaviorSubject<CardList | null>(null);
-  private cardSubject = new BehaviorSubject<ScryfallCard | null>(null);
-  private backendSubject = new BehaviorSubject<CardSearch | null>(null);
-  private printsSubject = new BehaviorSubject<ScryfallList | null>(null);
   private currentTotalSubject = new BehaviorSubject<number>(0);
-
   private errorMessageSubject = new BehaviorSubject<string>(
     'Default Error Message'
   );
 
-  constructor() {
-    const savedCard = sessionStorage.getItem('lastSearchedCard');
-    if (savedCard) {
-      this.backendSubject.next(JSON.parse(savedCard));
-    }
-  }
+  constructor() {}
 
+  // Subject for updating the current total price of the list
   getCurrentTotalListener() {
     return this.currentTotalSubject.asObservable();
   }
@@ -37,71 +22,9 @@ export class GlimpseStateService {
     this.currentTotalSubject.next(total);
   }
 
-  // get observables for other components
-  getBackendCardListener() {
-    return this.backendSubject.asObservable();
-  }
-
-  getUserListener() {
-    return this.userSubject.asObservable();
-  }
-
-  getUserListListener() {
-    return this.userListSubject.asObservable();
-  }
-
-  getCardListener() {
-    return this.cardSubject.asObservable();
-  }
-
-  getPrintsListener() {
-    return this.printsSubject.asObservable();
-  }
-
+  // Subject for passing the backend error message into the 404 page.
   getErrorMessageListener() {
     return this.errorMessageSubject.asObservable();
-  }
-
-  // get & set current values
-  setBackendCard(card: CardSearch | null) {
-    sessionStorage.setItem('lastSearchedCard', JSON.stringify(card));
-    this.backendSubject.next(card);
-  }
-
-  getUser() {
-    return this.userSubject.value;
-  }
-
-  setUser(user: User | null) {
-    this.userSubject.next(user);
-  }
-
-  getCurrentList() {
-    return this.userListSubject.value;
-  }
-
-  setCurrentList(list: CardList) {
-    this.userListSubject.next(list);
-  }
-
-  getSearchedCard() {
-    return this.cardSubject.value;
-  }
-
-  setSearchedCard(card: ScryfallCard) {
-    this.cardSubject.next(card);
-  }
-
-  getPrints() {
-    return this.printsSubject.value;
-  }
-
-  setSearchedPrints(prints: ScryfallList) {
-    this.printsSubject.next(prints);
-  }
-
-  getErrorMessage() {
-    return this.errorMessageSubject.value;
   }
 
   setErrorMessage(message: string) {
