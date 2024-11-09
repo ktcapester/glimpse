9d25b1675efc39298c8f9961a1f6ecf72c9f5926
Kevin Capener <kevincapener@gmail.com>
2024-11-08 16:17:12 -0800

    Added component for when there are too many results

---
+++ Glimpse/src/app/suggestions/suggestions.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { BackendGlueService } from '../services/backend-glue.service';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-suggestions',
+  standalone: true,
+  imports: [],
+  templateUrl: './suggestions.component.html',
+  styleUrl: './suggestions.component.css',
+})
+export class SuggestionsComponent implements OnInit, OnDestroy {
+  private destroy$ = new Subject<void>();
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private glue: BackendGlueService
+  ) {}
+
+  ngOnDestroy(): void {
+    console.log('SuggestionsComponent ngOnDestroy called!', Date.now());
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  ngOnInit(): void {
+    console.log('SuggestionsComponent ngOnInit called!', Date.now());
+    // Set up getting names out of the URL
+    this.route.paramMap.pipe(takeUntil(this.destroy$)).subscribe((params) => {
+      const term = params.get('term') || '';
+    });
+  }
+}
