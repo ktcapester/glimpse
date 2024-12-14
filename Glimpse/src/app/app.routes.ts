4aefa7b1ba6d010a755bd6259c0905549c55c8ee
Kevin Capener <kevincapener@gmail.com>
2024-12-13 16:17:29 -0800

    Added search term to suggestions route

--- Glimpse/src/app/app.routes.ts
+++ Glimpse/src/app/app.routes.ts
@@ -29,7 +29,7 @@ export const routes: Routes = [
     component: SearchResultComponent,
   },
   {
-    path: 'suggestions',
+    path: 'suggestions/:term',
     title: 'Glimpse',
     component: SuggestionsComponent,
   },
