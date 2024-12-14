a286d7994153c611ce1c779fe2cf5b1a6525e8b7
Kevin Capener <kevincapener@gmail.com>
2024-12-13 16:05:34 -0800

    Added new interfaces for backend response data

--- Glimpse/src/app/interfaces/backend.interface.ts
+++ Glimpse/src/app/interfaces/backend.interface.ts
@@ -1,6 +1,21 @@
+export interface CardDisplayOnly {
+  name: string;
+  imgsrc: string;
+  scryfall: string;
+}
+
 export interface CardSearch {
   name: string;
   imgsrc: string;
+  usd: number;
+  usd_etched: number;
+  usd_foil: number;
+  eur: number;
+  eur_etched: number;
+  eur_foil: number;
+}
+
+export interface CardPrices {
   usd: number;
   usd_etched: number;
   usd_foil: number;
