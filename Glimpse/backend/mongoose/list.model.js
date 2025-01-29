eef25dc0ff6875c94221d1b1d0b51a8994a0bcf9
Kevin Capener <kevincapener@gmail.com>
2025-01-29 10:48:20 -0800

    mongodb option timestamps handles createdAt/updatedAt stuff automatically for me

--- Glimpse/backend/mongoose/list.model.js
+++ Glimpse/backend/mongoose/list.model.js
@@ -1,26 +1,29 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-const listSchema = new Schema({
-  user: { type: Schema.Types.ObjectId, ref: "User", required: true },
-  name: { type: String, required: true, maxlength: 100 },
-  description: { type: String, required: true, maxlength: 500 },
-  totalPrice: { type: Number, default: 0 },
-  createdAt: { type: Date, default: Date.now, immutable: true },
-  updatedAt: { type: Date, default: Date.now },
-  cards: [
-    {
-      card: { type: Schema.Types.ObjectId, ref: "Card", required: true },
-      quantity: { type: Number, default: 1, min: 1 },
-    },
-  ],
-});
+const listSchema = new Schema(
+  {
+    user: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    name: { type: String, required: true, maxlength: 100 },
+    description: { type: String, required: true, maxlength: 500 },
+    totalPrice: { type: Number, default: 0 },
+    cards: [
+      {
+        card: { type: Schema.Types.ObjectId, ref: "Card", required: true },
+        quantity: { type: Number, default: 1, min: 1 },
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+// timestamps: true --> handles covering updatedAt on save.
 
 // Hook for logic that happens every time the List is saved
-listSchema.pre("save", async function (next) {
-  this.updatedAt = Date.now();
-  next();
-});
+// listSchema.pre("save", async function (next) {
+//   this.updatedAt = Date.now();
+//   next();
+// });
 
 // Add indexing for efficient querying
 listSchema.index({ user: 1 }); // used when querying for all lists belonging to a certain user
