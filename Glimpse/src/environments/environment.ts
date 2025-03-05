4eb85b98f039aa174265626af740ad7551cd4127
Kevin Capener <kevincapener@gmail.com>
2025-03-05 11:11:19 -0800

    fix: updated api url

--- Glimpse/src/environments/environment.ts
+++ Glimpse/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiURL: 'https://glimpsecard.com/api',
+  apiURL: 'https://api.glimpsecard.com/api',
   // apiURL: 'http://localhost:3000/api',
 };
 
