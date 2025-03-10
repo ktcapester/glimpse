29467b8067928d11ca4820b40835d295b7177a3a
Kevin Capener <kevincapener@gmail.com>
2025-03-10 14:51:22 -0700

    removed sample code

--- Glimpse/backend/mongoose/magiclink.route.js
+++ Glimpse/backend/mongoose/magiclink.route.js
@@ -10,9 +10,4 @@ router.post("/auth/magic-link", postMagicLink);
 // Endpoint to verify the token
 router.get("/auth/verify", getMagicToken);
 
-// Protected: Example of a protected route (e.g., user profile)
-router.get("/auth/protected", authenticateJWT, (req, res) => {
-  res.json({ message: "You are authenticated!", user: req.user });
-});
-
 module.exports = router;
