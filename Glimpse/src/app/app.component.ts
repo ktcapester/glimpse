a095e5c3e3aade53b48e4b3a91d8ecbaa1cb85eb
Kevin Capener <kevincapener@gmail.com>
2025-04-15 09:57:43 -0700

    restructured top level app-component to hold the header, the router outlet, and the footer

--- Glimpse/src/app/app.component.ts
+++ Glimpse/src/app/app.component.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
 })
 export class AppComponent {}
+
+/*
+add divs to app.html    DONE
+add app-header        DONE
+create app-footer     DONE
+add app-footer          DONE
+remove app-header from all other components   DONE
+update all CSS >_<    ***
+fix start-search CSS
+fix 404 css
+fix login css
+fix verify css
+
+
+*/
