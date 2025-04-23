9280018c64c4776649e4b31c898092ffd5d0dfb0
Kevin Capener <kevincapener@gmail.com>
2025-04-22 23:20:19 -0700

    made http response typing more legible

--- Glimpse/src/app/services/card-list.service.ts
+++ Glimpse/src/app/services/card-list.service.ts
@@ -6,6 +6,16 @@ import { environment } from 'src/environments/environment';
 import { CardSchema } from '../interfaces/schemas.interface';
 import { ListData } from '../interfaces/list-data';
 
+interface BackendListItem {
+  card: CardSchema;
+  quantity: number;
+}
+
+interface BackendListData {
+  list: BackendListItem[];
+  currentTotal: number;
+}
+
 @Injectable({
   providedIn: 'root',
 })
@@ -14,39 +24,31 @@ export class CardListService {
   private http = inject(HttpClient);
 
   getList(listID: string): Observable<ListData> {
-    return this.http
-      .get<{
-        list: { card: CardSchema; quantity: number }[];
-        currentTotal: number;
-      }>(`${this.apiUrl}/${listID}`)
-      .pipe(
-        map((response) => {
-          const simpleList = response.list.map((entry) => {
-            return {
-              id: entry.card._id,
-              name: entry.card.name,
-              price: entry.card.prices?.calc?.usd || 0,
-              count: entry.quantity,
-            };
-          });
-          return { list: simpleList, currentTotal: response.currentTotal };
-        }),
-        shareReplay({ bufferSize: 1, refCount: true }),
-        // catchError here keeps the async pipe in card-list-component alive
-        // the global interceptor is used for global reactions like 404 redirects
-        catchError(() =>
-          of({
-            list: [],
-            currentTotal: 0,
-          })
-        )
-      );
+    return this.http.get<BackendListData>(`${this.apiUrl}/${listID}`).pipe(
+      map((response) => {
+        const simpleList = response.list.map((entry) => {
+          return {
+            id: entry.card._id,
+            name: entry.card.name,
+            price: entry.card.prices?.calc?.usd || 0,
+            count: entry.quantity,
+          };
+        });
+        return { list: simpleList, currentTotal: response.currentTotal };
+      }),
+      shareReplay({ bufferSize: 1, refCount: true }),
+      // catchError here keeps the async pipe in card-list-component alive
+      // the global interceptor is used for global reactions like 404 redirects
+      catchError(() =>
+        of({
+          list: [],
+          currentTotal: 0,
+        })
+      )
+    );
   }
 
   deleteList(listID: string) {
-    return this.http.delete<{
-      list: { card: CardSchema; quantity: number }[];
-      currentTotal: number;
-    }>(`${this.apiUrl}/${listID}`);
+    return this.http.delete<BackendListData>(`${this.apiUrl}/${listID}`);
   }
 }
