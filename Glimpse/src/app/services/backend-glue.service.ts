9ebcb51be9fe607992b344df30427b0941ebd284
Kevin Capener <kevincapener@gmail.com>
2025-04-23 00:21:01 -0700

    removed unneeded code now that we have new services

--- Glimpse/src/app/services/backend-glue.service.ts
+++ Glimpse/src/app/services/backend-glue.service.ts
@@ -1,8 +1,7 @@
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
-import { CardDisplayOnly } from '../interfaces/backend.interface';
 
 @Injectable({
   providedIn: 'root',
@@ -37,12 +36,5 @@ export class BackendGlueService {
           return { success: true, data: response.token };
         })
       );
-  }
-
-  getCardSearch(cardName: string) {
-    let params = new HttpParams().set('name', cardName);
-    return this.http.get<CardDisplayOnly[]>(`${this.apiUrl}/search`, {
-      params,
-    });
   }
 }
--- Glimpse/src/app/services/glimpse-state.service.ts
+++ Glimpse/src/app/services/glimpse-state.service.ts
@@ -1,40 +1,16 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
-import { take, tap } from 'rxjs/operators';
-import { BackendGlueService } from './backend-glue.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class GlimpseStateService {
   // internals
-  private currentTotalSubject = new BehaviorSubject<number>(0);
   private errorMessageSubject = new BehaviorSubject<string>(
     'Default Error Message'
   );
 
-  constructor(private glue: BackendGlueService) {}
-
-  // Subject for updating the current total price of the list
-  getCurrentTotalListener() {
-    return this.currentTotalSubject.asObservable();
-  }
-
-  pushNewTotal(total: number) {
-    this.currentTotalSubject.next(total);
-  }
-
-  initTotal(listID: string) {
-    this.glue
-      .getCardList(listID)
-      .pipe(
-        take(1), // only need to get value once, then complete please.
-        tap((results) => {
-          this.pushNewTotal(results.currentTotal);
-        })
-      )
-      .subscribe();
-  }
+  constructor() {}
 
   // Subject for passing the backend error message into the 404 page.
   getErrorMessageListener() {
