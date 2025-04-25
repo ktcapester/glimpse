15d228355f2e0596c9bdf25af812e09f896bd67c
Kevin Capener <kevincapener@gmail.com>
2025-04-24 22:29:56 -0700

    Use StorageService in the authguard

--- Glimpse/src/app/user-auth.guard.ts
+++ Glimpse/src/app/user-auth.guard.ts
@@ -1,10 +1,12 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { jwtDecode } from 'jwt-decode';
+import { StorageService } from './services';
 
 export const userAuthGuard: CanActivateFn = (route, state) => {
-  const token = localStorage.getItem('jwtToken');
   const router = inject(Router);
+  const storage = inject(StorageService);
+  const token = storage.getItem('jwtToken');
 
   if (!token) {
     return router.parseUrl('/login');
