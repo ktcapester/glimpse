35b9aa01abd4ea6fe5efb1b253b020eb8984b6f2
Kevin Capener <kevincapener@gmail.com>
2025-05-02 14:43:56 -0700

    fixed header because its BG value is being handled in app.ts

--- Glimpse/src/app/header/header.component.ts
+++ Glimpse/src/app/header/header.component.ts
@@ -3,14 +3,9 @@ import {
   Component,
   inject,
   input,
-  OnDestroy,
-  OnInit,
 } from '@angular/core';
 import { SearchBarComponent } from '../search-bar/search-bar.component';
 import { Router } from '@angular/router';
-import { filter, tap, takeUntil } from 'rxjs/operators';
-import { Subject } from 'rxjs';
-import { narrowEventToNavigationEnd } from '../type-guard.util';
 import { NgOptimizedImage } from '@angular/common';
 
 @Component({
@@ -20,40 +15,9 @@ import { NgOptimizedImage } from '@angular/common';
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [SearchBarComponent, NgOptimizedImage],
 })
-export class HeaderComponent implements OnInit, OnDestroy {
+export class HeaderComponent {
   private router = inject(Router);
-  private sandColorRoutes = ['/login']; // defines which pages want the fake-margin to be sand color
-  backgroundClass = 'bg-sand';
-  private destroy$ = new Subject<void>();
   initBG = input('bg-sand'); // gets starting value from app-component
-
-  ngOnDestroy(): void {
-    this.destroy$.next();
-    this.destroy$.complete();
-  }
-
-  ngOnInit(): void {
-    this.backgroundClass = this.initBG(); // retrieve starting value
-
-    this.router.events
-      .pipe(
-        takeUntil(this.destroy$),
-        filter(narrowEventToNavigationEnd), // type guard from Router.Event to NavigationEnd
-        tap((event) => {
-          // change color of fake-margin depending on which component is loaded
-          if (this.sandColorRoutes.includes(event.urlAfterRedirects)) {
-            this.setColorToSand(true);
-          } else {
-            this.setColorToSand(false);
-          }
-        })
-      )
-      .subscribe();
-  }
-
-  setColorToSand(sandy: boolean) {
-    this.backgroundClass = sandy ? 'bg-sand' : 'bg-white';
-  }
 
   onLogoClick() {
     this.router.navigate(['']);
