13a8b5e6a6ee4441cff145a85d0120d9b4736340
Kevin Capener <kevincapener@gmail.com>
2025-05-16 10:52:41 -0700

    another step closer to making user state correct

--- Glimpse/backend/middleware/authJWT.middle.js
+++ Glimpse/backend/middleware/authJWT.middle.js
@@ -3,40 +3,36 @@
  * @module Middleware/AuthJWT
  */
 
-/**
- * Decoded JWT payload structure.
- * @typedef {Object} module:Middleware/AuthJWT~DecodedJWT
- * @property {string} userId - ID of the authenticated user.
- * @property {string} email - Email address of the authenticated user.
- * @property {number} iat - Issued at timestamp.
- * @property {number} exp - Expiration timestamp.
- */
-
 const jwt = require("jsonwebtoken");
 
 /**
  * Middleware to authenticate requests using JWT.
  * @function
  * @name module:Middleware/AuthJWT.authenticateJWT
- * @param {Express.Request & { user?: DecodedJWT }} req - Express request with optional user property.
+ * @param {Express.Request & { userId?: string }} req - Express request with optional userId property.
  * @param {Express.Response} res - Express response object.
  * @param {Function} next - Express next middleware function.
  * @returns {void} Responds with an error message if authentication fails, otherwise calls the next middleware.
  */
 function authenticateJWT(req, res, next) {
-  const token = req.headers.authorization?.split(" ")[1];
-
-  if (!token) {
-    return res.status(401).json({ error: "Unauthorized" });
+  const authHeader = req.headers.authorization;
+  if (!authHeader?.startsWith("Bearer ")) {
+    return res.status(401).json({ error: "Unauthorized: no token provided" });
   }
+  const token = authHeader.substring(7); // Remove "Bearer " from the token string;
 
-  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-    if (err) {
-      return res.status(403).json({ error: "Invalid token" });
+  try {
+    // throws if token is invalid or expired
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
+    req.userId = payload.userId;
+    return next();
+  } catch (error) {
+    if (error.name === "TokenExpiredError") {
+      return res.status(401).json({ error: "Access token expired" });
     }
-    req.user = user;
-    next();
-  });
+
+    return res.status(401).json({ error: "Invalid access token" });
+  }
 }
 
 module.exports = authenticateJWT;
