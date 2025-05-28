02237a84100ad85642ef759e11a6775502d07b5b
Kevin Capener <kevincapener@gmail.com>
2025-05-27 17:18:21 -0700

    upgrade card-list service to shape data needed for component

--- Glimpse/src/app/services/card-list.service.ts
+++ Glimpse/src/app/services/card-list.service.ts
@@ -3,7 +3,7 @@ import { inject, Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { catchError, map, shareReplay } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
-import { CardSchema, ListData } from '../interfaces';
+import { CardSchema, ListData, ListItem } from '../interfaces';
 
 interface BackendListItem {
   card: CardSchema;
@@ -22,32 +22,54 @@ export class CardListService {
   private apiUrl = `${environment.apiURL}/list`;
   private http = inject(HttpClient);
 
-  getList(listID: string): Observable<ListData> {
-    return this.http.get<BackendListData>(`${this.apiUrl}/${listID}`).pipe(
-      map((response) => {
-        const simpleList = response.list.map((entry) => {
-          return {
-            id: entry.card._id,
-            name: entry.card.name,
-            price: entry.card.prices?.calc?.usd || 0,
-            count: entry.quantity,
-          };
-        });
-        return { list: simpleList, currentTotal: response.currentTotal };
-      }),
-      shareReplay({ bufferSize: 1, refCount: true }),
-      // catchError here keeps the async pipe in card-list-component alive
-      // the global interceptor is used for global reactions like 404 redirects
-      catchError(() =>
-        of({
-          list: [],
-          currentTotal: 0,
-        })
-      )
-    );
+  private entryMapper(entry: BackendListItem): ListItem {
+    return {
+      id: entry.card._id,
+      name: entry.card.name,
+      price: entry.card.prices?.calc?.usd || 0,
+      count: entry.quantity,
+    };
   }
 
-  deleteList(listID: string) {
-    return this.http.delete<BackendListData>(`${this.apiUrl}/${listID}`);
+  getList(listID: string): Observable<ListData> {
+    return this.http
+      .get<BackendListData>(`${this.apiUrl}/${listID}`, {
+        withCredentials: true,
+      })
+      .pipe(
+        map((response) => {
+          const simpleList = response.list.map(this.entryMapper);
+          return { list: simpleList, currentTotal: response.currentTotal };
+        }),
+        shareReplay({ bufferSize: 1, refCount: true }),
+        // catchError here keeps the async pipe in card-list-component alive
+        // the global interceptor is used for global reactions like 404 redirects
+        catchError(() =>
+          of({
+            list: [],
+            currentTotal: 0,
+          })
+        )
+      );
+  }
+
+  deleteList(listID: string): Observable<ListData> {
+    return this.http
+      .delete<BackendListData>(`${this.apiUrl}/${listID}`, {
+        withCredentials: true,
+      })
+      .pipe(
+        map((response) => {
+          const simpleList = response.list.map(this.entryMapper);
+          return { list: simpleList, currentTotal: response.currentTotal };
+        }),
+        shareReplay({ bufferSize: 1, refCount: true }),
+        catchError(() =>
+          of({
+            list: [],
+            currentTotal: 0,
+          })
+        )
+      );
   }
 }
