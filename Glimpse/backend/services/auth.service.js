d207ea03dcc9cd229a4080aa6954dc729e2fdf0f
Kevin Capener <kevincapener@gmail.com>
2025-06-30 17:14:35 -0700

    implemented "sliding expiration" on refresh tokens. the TTL for the refresh token is updated each time a new access token is requested now.

--- Glimpse/backend/services/auth.service.js
+++ Glimpse/backend/services/auth.service.js
@@ -4,35 +4,40 @@ const { RefreshToken } = require("../models/refreshtoken.model");
 const { createError } = require("../utils");
 
 // ms * sec * min * hr * day
-const COOKIE_MAX_AGE = 1000 * 60 * 60 * 24 * 30; // 30 days
+const REFRESH_TOKEN_TTL_MS = 1000 * 60 * 60 * 24 * 30; // 30 days
 
-function generateAccessToken(userId) {
-  return jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: "15m" });
-}
+const ACCESS_TOKEN_TTL_MS = 1000 * 60 * 15; // 15 minutes
 
-async function createRefreshToken(userId) {
+const generateAccessToken = (userId) => {
+  return jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: `${ACCESS_TOKEN_TTL_MS}ms`,
+  });
+};
+
+const createRefreshToken = async (userId) => {
   const token = uuidv4();
-  const expiresAt = new Date(Date.now() + COOKIE_MAX_AGE);
+  const expiresAt = new Date(Date.now() + REFRESH_TOKEN_TTL_MS);
   await RefreshToken.create({
     token,
     userId,
     expiresAt,
   });
   return token;
-}
+};
 
-const refreshAccessToken = async (refreshToken) => {
-  const refreshDoc = await RefreshToken.findOne({ token: refreshToken });
-
-  if (!refreshDoc) {
-    throw createError(401, "Invalid refresh token.");
+const refreshTokens = async (refreshToken) => {
+  // "Sliding expiration": reset the expiration time when we find a valid token
+  const doc = await RefreshToken.findOneAndUpdate(
+    { token: refreshToken, expiresAt: { $gt: new Date() } },
+    { $set: { expiresAt: Date.now() + REFRESH_TOKEN_TTL_MS } },
+    { new: true }
+  );
+  if (!doc) {
+    throw createError(401, "Refresh token missing or expired.");
   }
 
-  if (refreshDoc.expiresAt < Date.now()) {
-    throw createError(401, "Refresh token has expired.");
-  }
-
-  return generateAccessToken(refreshDoc.userId);
+  // Issue new access token
+  return generateAccessToken(doc.userId);
 };
 
 const revokeRefreshToken = async (refreshToken) => {
@@ -40,6 +45,9 @@ const revokeRefreshToken = async (refreshToken) => {
 };
 
 module.exports = {
-  refreshAccessToken,
+  REFRESH_TOKEN_TTL_MS,
+  generateAccessToken,
+  createRefreshToken,
+  refreshTokens,
   revokeRefreshToken,
 };
