6bef1af643434033c1f0130f2cd631b768096c88
Kevin Capener <kevincapener@gmail.com>
2025-08-06 14:01:31 -0700

    added centralized constants for sharing between the front and back ends. mostly API response messages

---
+++ Glimpse/shared/constants/index.d.ts
@@ -0,0 +1,3 @@
+export declare const DELETE_USER_RESPONSE_STRING: string;
+export declare const CREATE_USER_RESPONSE_STRING: string;
+export declare const LOGOUT_SUCCESS_STRING: string;
---
+++ Glimpse/shared/constants/index.js
@@ -0,0 +1,3 @@
+exports.DELETE_USER_RESPONSE_STRING = "User deleted successfully";
+exports.CREATE_USER_RESPONSE_STRING = "User created successfully";
+exports.LOGOUT_SUCCESS_STRING = "Logged out successfully";
